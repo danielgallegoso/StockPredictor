245caba4031078500cbb6375da87f0be227be8bf
alicehau <alicehau@stanford.edu>
2015-11-13 16:25:57 -0800

    getting article links from bloomberg, ibmtimes, motley fool. added csv file of dates and links to parsing_files folder

--- getLinks.py
+++ getLinks.py
@@ -7,7 +7,7 @@ import re
 
 def parse_bloomberg(soup):
 	news = soup.find('div', attrs= {'class': 'news__state active'})
-	result = 'date,link\n'
+	result = ''
 	for story in news.find_all('article', attrs = {'class': 'news__story'}):
 		for headline in story.find_all('div', attrs = {'class': 'news__story__headline'}):
 			date = story.find('time')
@@ -27,9 +27,17 @@ def parse_fool(soup):
 		result += link[2:] + ',\n'
 	return result
 
-def parse(ibtimes(soup))
-
-
+def parse_ibtimes(soup):
+	result = ''
+	content = soup.find('ol', attrs={'class': 'archive-list apachesolr_search'})
+	headlines = content.find_all('li', attrs= {'class': 'clearfix'})
+	for headline in headlines:
+		date = headline.find('div', attrs= {'class':'byline'}).text
+		date_only = re.findall(r'\d\d\d\d-\d\d-\d\d', date)[0]
+		result += date_only + ','
+		link = headline.find('a')
+		result += link.get('href') + ',\n'
+	return result
 
 def parse():
 	scrape = open('scrapeLinks.json')
@@ -37,28 +45,32 @@ def parse():
 	scrape.close
 	sites = files.keys()
 	companies = ['apple', 'microsoft', 'amazon', 'tesla']
+	for company in companies:
+		filestream = open(company + '.csv', 'w+')
+		filestream.write('')
+		filestream.close()
 	for site in sites:
-		# for url in files[company]:
 		for company in companies:
-			url = files[site][company]
-			html = urllib2.urlopen(url).read()
-			soup = BeautifulSoup(html, "html.parser")
-			domain =  urlparse(url).netloc
-			
-			if domain == 'techcrunch.com':
-				content = parse_techcrunch(soup)
-			elif domain == 'www.bloomberg.com':
-				content = parse_bloomberg(soup)
-			elif domain == 'www.ibtimes.com':
-				content = parse_ibtimes(soup)
-			elif domain == 'www.fool.com':
-				content = parse_fool(soup)
+			urls = files[site][company]
+			for url in urls:
+				html = urllib2.urlopen(url).read()
+				soup = BeautifulSoup(html, "html.parser")
+				domain =  urlparse(url).netloc
+				
+				if domain == 'techcrunch.com':
+					content = parse_techcrunch(soup)
+				elif domain == 'www.bloomberg.com':
+					content = parse_bloomberg(soup)
+				elif domain == 'www.ibtimes.com':
+					content = parse_ibtimes(soup)
+				elif domain == 'www.fool.com':
+					content = parse_fool(soup)
 
-			filename = company + '.csv'
-			content = content.encode('ascii', 'ignore')
-			filestream = open(filename, 'a+')
-			filestream.write(content)
-			filestream.close()
+				filename = company + '.csv'
+				content = content.encode('ascii', 'ignore')
+				filestream = open(filename, 'a+')
+				filestream.write(content)
+				filestream.close()
 
 parse()
 
