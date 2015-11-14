7273e4f59393a4112cb7a26fd5b585b97a5e29ac
alicehau <alicehau@stanford.edu>
2015-11-14 15:11:49 -0800

    manually got tech crunch articles. standardized dates. added csv's of article links to parsing_files folder

--- getLinks.py
+++ getLinks.py
@@ -4,30 +4,36 @@ import json
 from urlparse import urlparse
 import hashlib
 import re
+import datetime
 
-def parse_bloomberg(soup):
+
+def parse_bloomberg(soup, company):
 	news = soup.find('div', attrs= {'class': 'news__state active'})
 	result = ''
 	for story in news.find_all('article', attrs = {'class': 'news__story'}):
 		for headline in story.find_all('div', attrs = {'class': 'news__story__headline'}):
 			date = story.find('time')
-			result += date.get('datetime') + ','
+			date_only = re.findall(r'\d\d\d\d-\d\d-\d\d', date.get('datetime'))[0]
+			result += date_only + ','
 			link = story.find('a')
-			result += link.get('href') + ',\n'
+			result += link.get('href') + ','
+			result += company + ',\n'
 	return result
 
-def parse_fool(soup):
+def parse_fool(soup, company):
 	articles = soup.find_all('article')
 	result = ''
 	for article in articles:
 		date = article.find('span')
 		date_only = re.findall(r'[A-Z][a-z][a-z] \d\d \d\d\d\d', date.text)[0]
+		date_only = datetime.datetime.strptime(date_only, '%b %d %Y').strftime('%Y-%m-%d')
 		result += date_only +','
 		link = article.find('a').get('href')
-		result += link[2:] + ',\n'
+		result += link[2:] + ','
+		result += company + ',\n'
 	return result
 
-def parse_ibtimes(soup):
+def parse_ibtimes(soup, company):
 	result = ''
 	content = soup.find('ol', attrs={'class': 'archive-list apachesolr_search'})
 	headlines = content.find_all('li', attrs= {'class': 'clearfix'})
@@ -36,7 +42,8 @@ def parse_ibtimes(soup):
 		date_only = re.findall(r'\d\d\d\d-\d\d-\d\d', date)[0]
 		result += date_only + ','
 		link = headline.find('a')
-		result += link.get('href') + ',\n'
+		result += link.get('href') + ','
+		result += company + ',\n'
 	return result
 
 def parse():
@@ -44,7 +51,7 @@ def parse():
 	files = json.load(scrape)
 	scrape.close
 	sites = files.keys()
-	companies = ['apple', 'microsoft', 'amazon', 'tesla']
+	companies = ['aapl', 'msft', 'amzn', 'tsla']
 	for company in companies:
 		filestream = open(company + '.csv', 'w+')
 		filestream.write('')
@@ -58,13 +65,13 @@ def parse():
 				domain =  urlparse(url).netloc
 				
 				if domain == 'techcrunch.com':
-					content = parse_techcrunch(soup)
+					content = parse_techcrunch(soup, company)
 				elif domain == 'www.bloomberg.com':
-					content = parse_bloomberg(soup)
+					content = parse_bloomberg(soup, company)
 				elif domain == 'www.ibtimes.com':
-					content = parse_ibtimes(soup)
+					content = parse_ibtimes(soup, company)
 				elif domain == 'www.fool.com':
-					content = parse_fool(soup)
+					content = parse_fool(soup, company)
 
 				filename = company + '.csv'
 				content = content.encode('ascii', 'ignore')
