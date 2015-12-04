f714ddfda1b6b3e5201289627aea766c0eb64208
Daniel Gallegos <dgalort@stanford.edu>
2015-12-04 14:53:14 -0800

    minor changes to allow stemming

--- frequency_table.py
+++ frequency_table.py
@@ -8,16 +8,17 @@ python frequency_table.py msft
 
 def main(argv):
 	freq = {}
-	for filename in os.listdir(os.getcwd() + '/' + argv[1] + '/raw'):
-		if filename.endswith('.DS_Store') is False:
-			entry = open(argv[1] + '/raw/' + filename, 'r')
-			tokens =  entry.read().split()
-			for token in tokens:
-				if freq.get(token) is None:
-					freq[token] = 1
-				else:
-					freq[token] += 1
-			entry.close()
+	for i in xrange(1,len(argv)):
+		for filename in os.listdir(os.getcwd() + '/' + argv[i] + '/stemmed'):
+			if filename.endswith('.DS_Store') is False:
+				entry = open(argv[1] + '/stemmed/' + filename, 'r')
+				tokens =  entry.read().split()
+				for token in tokens:
+					if freq.get(token) is None:
+						freq[token] = 1
+					else:
+						freq[token] += 1
+				entry.close()
 	keys = freq.keys()
 	keys = sorted(keys, key = lambda token: freq[token])
 	for key in keys:
---
+++ processor_stem_naive.py
@@ -0,0 +1,72 @@
+import os
+import sys
+import csv
+
+'''
+TODO: make csv for y's
+
+Based on dictionary.txt will create frequency csv
+
+python processor.py msft
+
+'''
+
+
+def get_freq_array(dictionary, filename):
+	result = []
+	entry = open(filename, 'r')
+	tokens =  entry.read().split()
+	entry.close()
+	for i in xrange(0,len(dictionary)):
+		result.append(0)
+		for token in tokens:
+			if token == dictionary[i]:
+				result[i] += 1
+	return result, tokens[0]
+
+	
+def get_prices():
+	csvfile = open('prices.csv', 'rU')
+	reader = csv.reader(csvfile, delimiter = ',', dialect=csv.excel_tab)
+	prices = {}
+	header = next(reader, None)
+	for i in xrange(1, len(header)):
+		prices[header[i]] = {}
+	prev = None
+	for row in reader:
+		for i in xrange(1, len(row)):
+			prices[header[i]][row[0]] = row[i]
+	csvfile.close()
+	return prices
+
+
+
+def main(argv):
+	dictfile = open('dictionary.txt', 'r+')
+	dictionary = dictfile.read().split()
+	dictfile.close()
+	prices = get_prices()
+	rows = []
+	ys = []
+	for filename in os.listdir(os.getcwd() + '/' + argv[1] + '/stemmed'):
+		if filename.endswith('.DS_Store') is False:
+			row, date = get_freq_array(dictionary, argv[1] + '/stemmed/' + filename)
+			ys.append(prices[argv[1]][date])
+			rows.append(row)
+
+	# rows = zip(*rows)
+
+	csvfile = open(argv[1] + '/x.csv', 'wb')
+	writer = csv.writer(csvfile, delimiter = ',')
+	for row in rows:
+		writer.writerow(row)
+	csvfile.close()
+	csvfile = open(argv[1] + '/y.csv', 'wb')
+	csv.writer(csvfile, delimiter = ',').writerow(ys)
+	csvfile.close()
+
+			
+			
+
+if __name__ == "__main__":
+    main(sys.argv)
